ae0a7806adc3f3c7751a3ba37fcf53349b7761c9
huutai <truongtai137@gmail.com>
2021-09-23 02:13:34 +0700

    Delete some files

--- launch_blender.ps1
+++
@@ -1,6 +0,0 @@
-
-$BLENDER="C:\\Program Files\\Blender Foundation\\Blender 2.93\\blender.exe"
-$FILE=".\test_file.blend"
-
-# Stop-Process -Name blender
-& $BLENDER $FILE --python custom_render_engine.py --window-geometry 0 0 900 1000
