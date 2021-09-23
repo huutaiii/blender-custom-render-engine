297991bd1d287e921998016fcb52a94751972199
huutai <truongtai137@gmail.com>
2021-09-23 09:59:29 +0700

    Include only source files in zip

--- make_zip.sh
+++ make_zip.sh
@@ -5,8 +5,11 @@ which zip >/dev/null || { echo Please install zip; exit 1; }
 SOURCE=src
 TARGET=custom_render_engine
 
+rm $TARGET -r
+rm $TARGET.zip
+
 echo Installing files
-find $SOURCE -type f | while read file
+find $SOURCE -type f -name '*.py' -o -name '*.glsl' | while read file
 do
     echo $file
     install -D $file ${file/$SOURCE/$TARGET}
